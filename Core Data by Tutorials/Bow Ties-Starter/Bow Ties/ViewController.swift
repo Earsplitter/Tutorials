7d24aa6290076ae349a28d2aa355e1304529a404
Brian Mendez <brmendez@gmail.com>
2015-01-26 19:36:23 -0800

    Added first bowtie

--- Core Data by Tutorials/Bow Ties-Starter/Bow Ties/ViewController.swift
+++ Core Data by Tutorials/Bow Ties-Starter/Bow Ties/ViewController.swift
@@ -7,10 +7,9 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
-    
-    //test
   
   @IBOutlet weak var segmentedControl: UISegmentedControl!
   @IBOutlet weak var imageView: UIImageView!
@@ -19,10 +18,30 @@ class ViewController: UIViewController {
   @IBOutlet weak var timesWornLabel: UILabel!
   @IBOutlet weak var lastWornLabel: UILabel!
   @IBOutlet weak var favoriteLabel: UILabel!
+    var managedContext: NSManagedObjectContext!
+    var currentBowtie: Bowtie!
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+
+    insertSampleData()
+    
+    let request = NSFetchRequest(entityName: "Bowtie")
+    
+    let firstTitle = segmentedControl.titleForSegmentAtIndex(0)
+    
+    request.predicate = NSPredicate(format: "searchKey == %@", firstTitle!)
+    
+    var error: NSError? = nil
+    
+    var results = managedContext.executeFetchRequest(request, error: &error) as [Bowtie]?
+    
+    if let bowties = results {
+        currentBowtie = bowties[0]
+        populate(currentBowtie)
+    } else {
+        println("Could not fetch \(error), \(error!.userInfo)")
+    }
     
   }
   
@@ -37,5 +56,80 @@ class ViewController: UIViewController {
   @IBAction func rate(sender: AnyObject) {
     
   }
+    
+    func insertSampleData() {
+        
+        let fetchRequest = NSFetchRequest(entityName: "Bowtie")
+        fetchRequest.predicate = NSPredicate(format: "searchKey != nil")
+        
+        let count = managedContext.countForFetchRequest(fetchRequest, error: nil)
+        
+        if count > 0 { return }
+        
+        let path = NSBundle.mainBundle().pathForResource("SampleData", ofType: "plist")
+        
+        let dataArray = NSArray(contentsOfFile: path!)!
+        
+        for dict : AnyObject in dataArray {
+            
+            let entity = NSEntityDescription.entityForName("Bowtie", inManagedObjectContext: managedContext)
+            let bowtie = Bowtie(entity: entity!, insertIntoManagedObjectContext: managedContext)
+            
+            let btDict = dict as NSDictionary
+            bowtie.name = btDict["name"] as NSString
+            bowtie.searchKey = btDict["searchKey"] as NSString
+            bowtie.rating = btDict["rating"] as NSNumber
+            let tintColorDict = btDict["tintColor"] as NSDictionary
+            bowtie.tintColor = colorFromDict(tintColorDict)
+            
+            let imageName = btDict["imageName"] as NSString
+            let image = UIImage(named: imageName)
+            let photoData = UIImagePNGRepresentation(image)
+            bowtie.photoData = photoData
+            
+            bowtie.lastWorn = btDict["lastWorn"] as NSDate
+            bowtie.timesWorn = btDict["timesWorn"] as NSNumber
+            bowtie.isFavorite = btDict["isFavorite"] as NSNumber
+            
+        }
+        
+        var error: NSError?
+        if !managedContext.save(&error) {
+            println("Could not save \(error), \(error!.userInfo)")
+        }
+    }
+    
+    func colorFromDict(dict: NSDictionary) -> UIColor {
+        
+        let red = dict["red"] as NSNumber
+        let green = dict["green"] as NSNumber
+        let blue = dict["blue"] as NSNumber
+        
+        let color = UIColor(red: CGFloat(red)/255.0, green: CGFloat(green)/255.0, blue: CGFloat(blue)/255.0, alpha: 1)
+
+        return color
+        
+    }
+    
+    func populate(bowtie: Bowtie) {
+        
+        imageView.image = UIImage(data: bowtie.photoData)
+        nameLabel.text = bowtie.name
+        ratingLabel.text = "Rating: \(bowtie.rating.doubleValue)/5"
+        
+        timesWornLabel.text = "# times worn: \(bowtie.timesWorn.integerValue)"
+        
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateStyle = .ShortStyle
+        dateFormatter.timeStyle = .NoStyle
+        
+        lastWornLabel.text = "Last worn: " + dateFormatter.stringFromDate(bowtie.lastWorn)
+        
+        favoriteLabel.hidden = !bowtie.isFavorite.boolValue
+        
+        view.tintColor = bowtie.tintColor as UIColor
+        
+    }
+    
 }
 
